26dbe6febbfe31279d2a29278a2400f71d04643a
Evgeniy Nakoneschniy <e.nakoneschniy@gmail.com>
2016-10-01 09:36:55 +0300

    lesson 02

--- 02/index.php
+++ 02/index.php
@@ -1,3 +1,12 @@
-<?php
+<?
 echo "<h1>Hello World</h1>";
-echo "<h2>Hello World</h2>";
+
+$name = "Петя";
+$user_name = 'Меня зовут: $name';
+$user_Name = 'admin';
+
+//$вася = "Вася";//так не писать
+
+?>
+
+<span>Меня зовут: <strong><?=$name?></strong></span>
