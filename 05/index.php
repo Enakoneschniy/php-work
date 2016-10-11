6132cd95fe6098761766738d2e2a0db03fd08803
Evgeniy Nakoneschniy <e.nakoneschniy@gmail.com>
2016-10-11 20:53:18 +0300

    lesson 05

---
+++ 05/index.php
@@ -0,0 +1,146 @@
+<?php
+$dictionary = [
+    "car" => "Автомобиль",
+    "cat" => "Кошка",
+    "dog" => "Пес",
+    "apple" => "Яблоко"
+];
+
+echo $dictionary["cat"];
+
+var_dump($dictionary);
+
+echo "<hr>";
+
+$dictionary = [
+    "car" => ["Автомобиль", "Машина"],
+    "cat" => ["Кошка", "Кот"],
+    "dog" => ["Пес", "Собака"],
+    "apple" => "Яблоко"
+];
+
+echo $dictionary['car'][0];
+echo $dictionary['car'][1];
+
+var_dump($dictionary);
+
+echo "<hr>";
+
+$dictionary = [
+    "car" => ["Автомобиль", "Машина"],
+    "cat" => ["Кошка", "Кот"],
+    "dog" => ["Пес", "Собака"],
+    "apple" => "Яблоко"
+];
+?>
+<form>
+    <input type="text" name="word">
+    <input type="submit" value="Translate">
+</form>
+<?php
+var_dump($_REQUEST);
+$word = $_REQUEST["word"];
+$traslate_word = $dictionary[$word];
+if(is_array($traslate_word)){
+    foreach ($traslate_word as $word) {
+        echo $word,"<br>";
+    }
+}else{
+    echo $traslate_word;
+}
+
+echo "<hr>";
+
+$users = [
+    [
+        "name" => "Вася",
+        "age" => 45,
+        "gender" => "Мужской"
+    ],
+    [
+        "name" => "Оксана",
+        "age" => 21,
+        "gender" => "Женский"
+    ],
+    [
+        "name" => "Петя",
+        "age" => 33,
+        "gender" => "Мужской"
+    ],
+    [
+        "name" => "Катя",
+        "age" => 24,
+        "gender" => "Женский"
+    ]
+];
+?>
+
+<table border="1" cellspacing="0">
+    <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Пол</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($users as $user):?>
+            <tr>
+                <td><?=$user['name']?></td>
+                <td><?=$user['age']?></td>
+                <td><?=$user['gender']?></td>
+            </tr>
+        <?php endforeach;?>
+    </tbody>
+</table>
+<?php
+$dictionary = [
+    "car" => ["Автомобиль", "Машина"],
+    "cat" => ["Кошка", "Кот"],
+    "dog" => ["Пес", "Собака"],
+    "apple" => "Яблоко"
+];
+
+$dictionary["human"] = ["Человек", "Существо разумное"];
+
+$dictionary[] = "Блаблабла";
+$dictionary[] = "sdgfdsfsdf";
+
+var_dump($dictionary);
+
+unset($dictionary['car'][0], $dictionary['car'][1]);
+
+var_dump($dictionary);
+
+echo "<hr>";
+
+$products = [
+    [
+        "name" => "Iphone 7",
+        "price" => 600,
+        "properties" => [
+            [
+                "name" => "Processor",
+                "value" => "i10"
+            ],
+            [
+                "name" => "Display",
+                "value" => "5,5"
+            ]
+        ]
+    ],
+    [
+        "name" => "Iphone 6",
+        "price" => 500,
+        "properties" => [
+            [
+                "name" => "Processor",
+                "value" => "i9"
+            ],
+            [
+                "name" => "Display",
+                "value" => "5,2"
+            ]
+        ]
+    ]
+];
