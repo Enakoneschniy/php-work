ee96b9c8271760c6f14d7b3146e394d016d6a0e3
Evgeniy Nakoneschniy <e.nakoneschniy@gmail.com>
2016-11-26 11:48:20 +0200

    lesson 15

--- 16-17/classes/Storage.php
+++ 16-17/classes/Storage.php
@@ -1,5 +1,27 @@
 <?php
 
 class Storage {
+    private $list = [];
+    private $storagePath = '';
 
+    public function __construct($storagePath){
+        $this->storagePath = $storagePath;
+        //load from file
+    }
+
+    public function __destruct(){
+        $this->save();
+    }
+
+    public function add(Review $review){// add new review
+        $this->list[] = $review;
+    }
+
+    public function save(){//save review to file
+
+    }
+
+    public function load(){//load reviews from file
+
+    }
 }
